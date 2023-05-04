f0e88527c377c9376e16e2fd457f02ace1cb1157
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-05-04 01:47:49 -0700

    Repetition Token #1

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -4,7 +4,7 @@
 str = ARGV[0]
 
 # Use a regular expression to find the first occurrence of "htn", "hbtn", "hbbtn", or "hbbbtn"
-match = str.scan(/hb*tn/)
+match = str.scan(/hb?tn/)
 
 # Print the matching substring or an error message
 match.each do |match|
