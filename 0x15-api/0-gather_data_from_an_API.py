dbaba63bc1cf769b73fc529fa979fc850ca0ca50
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-07-24 04:43:28 -0700

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,50 +1,33 @@
 #!/usr/bin/python3
 """
-Returns info about employee TODO progress.
+API - https://jsonplaceholder.typicode.com
+returns info about employee TODO progress
 """
-
 import re
 import requests
 import sys
 
+
 API = "https://jsonplaceholder.typicode.com"
+"""REST API url"""
 
-def get_employee_todo_progress(employee_id):
-    """
-    Get employee TODO progress and display it.
-
-    Args:
-        employee_id (int): The ID of the employee.
-
-    Returns:
-        None
-    """
-    # Get employee information
-    employee_info_url = f"{API}/users/{employee_id}"
-    employee_info_response = requests.get(employee_info_url)
-    employee_info = employee_info_response.json()
-    EMPLOYEE_NAME = employee_info.get('name')
-
-    # Get TODO list for the employee
-    todos_url = f"{API}/todos?userId={employee_id}"
-    todos_response = requests.get(todos_url)
-    todos = todos_response.json()
-
-    # Filter completed tasks
-    completed_tasks = [todo for todo in todos if todo.get('completed')]
-    NUMBER_OF_DONE_TASKS = len(completed_tasks)
-    TOTAL_NUMBER_OF_TASKS = len(todos)
-
-    # Display progress information
-    print(f'Employee {EMPLOYEE_NAME} is done with tasks ({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
-    for task in completed_tasks:
-        TASK_TITLE = task.get("title")
-        print(f'\t {TASK_TITLE}')
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2 and sys.argv[1].isdigit():
-        EMPLOYEE_ID = int(sys.argv[1])
-        get_employee_todo_progress(EMPLOYEE_ID)
-    else:
-        print("Usage: python script_name.py <employee_id>")
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            user_res = requests.get('{}/users/{}'.format(API, id)).json()
+            todos_res = requests.get('{}/todos'.format(API)).json()
+            user_name = user_res.get('name')
+            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+            todos_done = list(filter(lambda x: x.get('completed'), todos))
+            print(
+                'Employee {}: is done with tasks({}/{}):'.format(
+                    user_name,
+                    len(todos_done),
+                    len(todos)
+                )
+            )
+            for todo_done in todos_done:
+                print('\t{}'.format(todo_done.get('title')))
 
