dbe30ecf03a8847755cdd203b0e84a8fa68fe5d5
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-07-24 05:25:30 -0700

    Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+API - https://jsonplaceholder.typicode.com
+gathers data from API and exports it to JSON file
+"""
+import json
+import requests
+
+API = "https://jsonplaceholder.typicode.com"
+"""REST API url"""
+
+
+def get_employee_todo_data(employee_id):
+    """
+    Get employee's TODO list data.
+
+    Args:
+        employee_id (int): The ID of the employee.
+
+    Returns:
+        list: List of TODO items owned by the employee.
+    """
+    todos_url = f"{API}/todos?userId={employee_id}"
+    todos_response = requests.get(todos_url).json()
+    return todos_response
+
+
+def export_all_employees_todo_to_json():
+    """
+    Export TODO data for all employees to a JSON file.
+
+    Returns:
+        None
+    """
+    all_employee_data = {}
+
+    users_response = requests.get(f"{API}/users").json()
+
+    for user in users_response:
+        user_id = user.get('id')
+        username = user.get('username')
+        todos = get_employee_todo_data(user_id)
+
+        user_data = []
+        for todo in todos:
+            task_title = todo.get("title")
+            completed_status = todo.get("completed")
+            user_data.append({"username": username, "task": task_title, "completed": completed_status})
+
+        all_employee_data[str(user_id)] = user_data
+
+    with open("todo_all_employees.json", 'w') as json_file:
+        json.dump(all_employee_data, json_file)
+
+
+if __name__ == '__main__':
+    export_all_employees_todo_to_json()
+
