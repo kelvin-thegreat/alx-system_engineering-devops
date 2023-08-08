69f95cdf0bc8ec8070b122a32f066bf0b26f03e5
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-08-08 13:41:49 -0700

    function that queries the Reddit API/returns the number of subscribers/prints the titles of the first 10 hot posts listed

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python3
-""" function that queries the Reddit API and prints the titles of the first 10 hot post """
+"""Reddit Top Ten Hottest Posts"""
+
 import requests
 
-def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
-    headers = {"User-Agent": "0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)"}
-    params = {"limit": 10}
+def get_top_ten_posts(subreddit_name):
+    """
+    Fetches and prints the titles of the 10 hottest posts on a given subreddit.
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-
-    if response.status_code != 200:
-        print("None")
+    Args:
+        subreddit_name (str): The name of the subreddit to retrieve posts from.
+    """
+    base_url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit_name)
+    headers = {
+        "User-Agent": "Reddit API Client v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    params = {
+        "limit": 10
+    }
+    
+    response = requests.get(base_url, headers=headers, params=params, allow_redirects=False)
+    
+    if response.status_code == 404:
+        print("Subreddit not found.")
         return
-
-    posts = response.json().get("data", {}).get("children", [])
-
-    for post in posts:
-        title = post.get("data", {}).get("title")
-        if title:
-            print(title)
+    
+    posts_data = response.json().get("data")
+    
+    for post in posts_data.get("children"):
+        post_title = post.get("data").get("title")
+        print(post_title)
 
 """ Example usage """
-subreddit_name = "programming"
-print(f"Top 10 hot posts on r/{subreddit_name}:")
-top_ten(subreddit_name)
+if __name__ == "__main__":
+    subreddit_to_check = "programming"
+    get_top_ten_posts(subreddit_to_check)
 
