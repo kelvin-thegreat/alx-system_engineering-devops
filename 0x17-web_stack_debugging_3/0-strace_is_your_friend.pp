0526549a6d88294aae60da20d531e5efc5b33a3a
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-08-09 16:12:04 -0700

    web_stack_debugging 3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -4,10 +4,12 @@
 #  content => template('my_module/wp-settings.php.erb'),
 #  require => Exec['fix-wordpress'],
 # }
-
-exec { 'fix-wordpress':
-  command     => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path        => '/usr/local/bin/:/bin/'
-
 }
-
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
+service { 'apache2':
+  ensure => 'running',
+  notify => Exec['fix-wordpress'],
+}
