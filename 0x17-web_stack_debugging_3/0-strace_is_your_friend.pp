1c59005d2f334d10bc55c39d1ab6d10224745c9e
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-08-09 04:14:45 -0700

    0x17-web_stack_debugging_3/fixing Word Press site

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,13 @@
+# Fix bad .phpp to .php : Why Apache is returning a 500 error
+# file { '/var/www/html/wp-settings.php':
+#  ensure  => file,
+#  content => template('my_module/wp-settings.php.erb'),
+#  require => Exec['fix-wordpress'],
+# }
+
+exec { 'fix-wordpress':
+  command     => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path        => '/usr/local/bin/:/bin/'
+
+}
+
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,2 @@
+# 0x17. Web stack debugging #3
+-- Debugging a web stack (Wordpress website) running on a LAMP stack.
